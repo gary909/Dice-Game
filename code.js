87c6940ae30c4f21e588b2a739e4014db44d729f
gary <gary.white@lodgit.com>
2021-11-11 16:14:01 +0100

    code semi-working

--- code.js
+++ code.js
@@ -1,17 +1,20 @@
 var p1dice = 0;
 var p2dice = 0;
 
+const newText = document.getElementById("newText");
+// newText.textContent = "Greetings from Mark";ss
+
 //var x = document.getElementById("message").innerHTML;
 
 function getRandomInt(max) {
     p1dice = Math.floor(Math.random() * max);
     p2dice = Math.floor(Math.random() * max);
     if (p1dice > p2dice){
-        return "Player 1 wins! \n" + "P1 scored: " + p1dice + "\nP2 scored: " + p2dice;
+        newText.textContent = "Player 1 wins! \n" + "P1 scored: " + p1dice + "\nP2 scored: " + p2dice;
     } else if (p2dice > p1dice) {
-        return "Player 2 wins! \n" + "P1 scored: " +  p1dice + "\nP2 scored: " + p2dice;
+        newText.textContent = "Player 2 wins! \n" + "P1 scored: " +  p1dice + "\nP2 scored: " + p2dice;
     } else {
-        return "It's a draw! \n" + "P1 scored: " +  p1dice + "\nP2 scored: " + p2dice;
+        newText.textContent = "It's a draw! \n" + "P1 scored: " +  p1dice + "\nP2 scored: " + p2dice;
     }
 }
 
