74029b6af848f0199bef7850bf4aba960a62adb4
gary <gary.white@lodgit.com>
2021-11-11 17:29:41 +0100

    code working

--- code.js
+++ code.js
@@ -3,15 +3,27 @@ var p2dice = 0;
 
 const newText = document.getElementById("newText");
 
-function getRandomInt(max) {
-    p1dice = Math.floor(Math.random() * max);
-    p2dice = Math.floor(Math.random() * max);
+const diceImg1 = document.getElementById("diceImg1");
+const diceImg2 = document.getElementById("diceImg2");
+
+var myArr = [diceImg2, diceImg1];
+
+function getRandomInt() {
+    p1dice = Math.floor(Math.random() * 6) + 1;
+    p2dice = Math.floor(Math.random() * 6) + 1;
+    // diceImg1 = 
     if (p1dice > p2dice){
-        newText.textContent = "Player 1 wins! \n" + "P1 scored: " + p1dice + "\nP2 scored: " + p2dice;
+        newText.innerHTML = "Player 1 wins! <br> <br> " + "(P1) scored: " + p1dice + " - & - (P2) scored: " + p2dice;
+        document.getElementById("diceImg1").src = diceImg1.src.replace("dice1.png", "dice" + p1dice + ".png");
+        document.getElementById("diceImg2").src = diceImg2.src.replace("dice2.png", "dice" + p2dice + ".png");
     } else if (p2dice > p1dice) {
-        newText.textContent = "Player 2 wins! \n" + "P1 scored: " +  p1dice + "\nP2 scored: " + p2dice;
+        newText.innerHTML = "Player 2 wins! <br> <br> " + "(P1) scored: " +  p1dice + " - & - (P2) scored: " + p2dice;
+        document.getElementById("diceImg1").src = diceImg1.src.replace("dice1.png", "dice" + p1dice + ".png");
+        document.getElementById("diceImg2").src = diceImg2.src.replace("dice2.png", "dice" + p2dice + ".png");
     } else {
-        newText.textContent = "It's a draw! \n" + "P1 scored: " +  p1dice + "\nP2 scored: " + p2dice;
+        newText.innerHTML = "It's a draw! <br> <br> " + "(P1) scored: " +  p1dice + " - & - (P2) scored: " + p2dice;
+        document.getElementById("diceImg1").src = diceImg1.src.replace("dice1.png", "dice" + p1dice + ".png");
+        document.getElementById("diceImg2").src = diceImg2.src.replace("dice2.png", "dice" + p2dice + ".png");
     }
 }
 
